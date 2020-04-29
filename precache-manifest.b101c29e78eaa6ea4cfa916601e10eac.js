4d5e52002e941ed35c6491536c7649fb9b9ba64a
Quentin V <quentin.vilchez@gmail.com>
2020-04-29 22:52:35 +0100

    Updates

---
+++ precache-manifest.b101c29e78eaa6ea4cfa916601e10eac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a49f05d0e3a5f26e688c023f91dfa66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "407c58475fa5bb9a3b8a",
+    "url": "/static/css/main.330bb43f.chunk.css"
+  },
+  {
+    "revision": "ec9d698fa7a0b289dad8",
+    "url": "/static/js/2.998ac811.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.998ac811.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "407c58475fa5bb9a3b8a",
+    "url": "/static/js/main.57d90204.chunk.js"
+  },
+  {
+    "revision": "0b4f3269348c2e5315e5",
+    "url": "/static/js/runtime-main.9089ee33.js"
+  },
+  {
+    "revision": "9a14056525dd9a16be65620b0688f8ba",
+    "url": "/static/media/loginGif.9a140565.gif"
+  },
+  {
+    "revision": "2d2efa22bc665e1faecd086b48bc594b",
+    "url": "/static/media/profile_pic.2d2efa22.jpg"
+  }
+]);
